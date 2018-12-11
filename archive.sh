e65df84dc0dae2e7fb8dacb7afebb7d88ab28ad3
David Mas <david.mas@irbbarcelona.org>
2018-12-11 18:12:11 +0100

    first commit

---
+++ archive.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+## usage
+# archive.sh folder
+
+## info 
+# it generates a tar compresed file (gz) with the last date of a modified 
+# file from that folder. This is very helpful to archive folders. 
+
+# script to archive a folder
+FOLDER=$(realpath -s $1) # this should also remove last slash
+
+lastDate=$(find $FOLDER -printf "%TY%Tm%Td\n" | sort -nr | head -n 1)
+
+DIR=$(basename $FOLDER)
+
+tar -zcvf $lastDate-$DIR.tar.gz $DIR
+
